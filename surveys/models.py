75f5df3c8e3c9b3d319edf372a8471284be774f9
Nymfeparakit <maxlex047@gmail.com>
2021-07-15 17:24:27 +0300

    Добавлены модели QuestionChoice, UserAnswersQuestion

--- surveys/models.py
+++ surveys/models.py
@@ -27,3 +27,31 @@ class Question(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class QuestionChoice(models.Model):
+    """
+    Вариант ответа для вопроса
+    """
+    question_id = models.ForeignKey(
+        'Question',
+        on_delete=models.CASCADE
+    )
+    text = models.CharField(max_length=250)
+
+
+class UserAnswersQuestion(models.Model):
+    """
+    Содержит информацию о выбранных пользователем вариантах ответа 
+    Или написанном тексте ответа
+    """
+    user_id = models.IntegerField()
+    question_id = models.ForeignKey(
+        'Question',
+        on_delete=models.CASCADE
+    )
+    choice_id = models.ForeignKey(
+        'QuestionChoice',
+        on_delete=models.CASCADE
+    ) # выбранный ответ, если вопроса содержал варианты ответа
+    answer_text = models.TextField() # текст ответа, если для вопроса не было вариантов ответа
