2bb33d2c172c91dd008d0b56ced9d67ea929e2c3
Nymfeparakit <maxlex047@gmail.com>
2021-07-16 14:00:45 +0300

    Добавлены ограничения для SurveyViewSet, QuestionViewSet на основе групп пользователей

--- surveys/views.py
+++ surveys/views.py
@@ -1,15 +1,28 @@
+import datetime
 from rest_framework import viewsets
 from rest_framework.permissions import IsAdminUser
+from datetime import date
 
 from .models import Survey, Question
 from .serializers import SurveySerializer, QuestionSerializer
 
 
 class SurveyViewSet(viewsets.ModelViewSet):
-    queryset = Survey.objects.all()
     serializer_class = SurveySerializer
+
+    def get_queryset(self):
+        """
+        Возвращает queryset для объектов опроса
+        Администраторы видят все объекты опросов
+        Остальные пользователи видят только активные (с актуальной датой окончания)
+        """
+        qs = Survey.objects.all()
+        if self.request.user.is_superuser:
+            return qs
+        return qs.filter(end_date__gte=date.today())
 
 
 class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
+    permission_classes = [IsAdminUser]
