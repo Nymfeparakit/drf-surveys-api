a019b3d95a3a9ec17ef75d6eb34b9627971ee3e9
Nymfeparakit <maxlex047@gmail.com>
2021-07-16 15:13:34 +0300

    Добавлена авторизация по токену

--- fabrique_project/urls.py
+++ fabrique_project/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'api/', include('surveys.urls')),
+    path('api/', include('surveys.urls')),
+    path('api-token-auth/', obtain_auth_token),
 ]
