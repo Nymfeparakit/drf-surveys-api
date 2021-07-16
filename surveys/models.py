ebdc29c9ea3a19fb8c374bc247ed41a3beaa3ade
Nymfeparakit <maxlex047@gmail.com>
2021-07-16 12:08:44 +0300

    Описание опроса теперь не обязательное поле

--- surveys/models.py
+++ surveys/models.py
@@ -8,7 +8,7 @@ class Survey(models.Model):
     title = models.CharField(max_length=250)
     start_date = models.DateTimeField() # дата начала
     end_date = models.DateTimeField() # дата конца
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.title
