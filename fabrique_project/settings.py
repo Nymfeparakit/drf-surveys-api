4b455c26f1e240affeda709014e05352daf1994a
Nymfeparakit <maxlex047@gmail.com>
2021-07-16 12:07:33 +0300

    Добавлены приложения

--- fabrique_project/settings.py
+++ fabrique_project/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # сторонние пакеты
+    'rest_framework',
+
+    # свои приложения
+    'surveys',
 ]
 
 MIDDLEWARE = [
