70ff55805826b11e0c07d2b75b9596862c13a200
Nymfeparakit <maxlex047@gmail.com>
2021-07-18 23:34:58 +0300

    Снова добавлены ендпоинты для пользователей

--- surveys/urls.py
+++ surveys/urls.py
@@ -8,14 +8,20 @@ from .views import SurveyViewSet, QuestionViewSet, TakeSurveyView, SimpleUserVie
 
 surveys_router = SimpleRouter()
 surveys_router.register(r'surveys', SurveyViewSet, basename='surveys')
+
 questions_router = routers.NestedSimpleRouter(surveys_router, r'surveys', lookup='survey')
 questions_router.register(r'questions', QuestionViewSet, basename='questions')
+
 choices_router = routers.NestedSimpleRouter(questions_router, r'questions', lookup='question')
 choices_router.register(r'choices', QuesionChoiceViewSet, basename='choices')
+
+users_router = SimpleRouter()
+users_router.register(r'users', SimpleUserView, basename='users')
 
 urlpatterns = [
     path('', include(surveys_router.urls)),
     path('', include(questions_router.urls)),
     path('', include(choices_router.urls)),
+    path('', include(users_router.urls)),
     path('survey_takes', TakeSurveyView.as_view()),
 ]
